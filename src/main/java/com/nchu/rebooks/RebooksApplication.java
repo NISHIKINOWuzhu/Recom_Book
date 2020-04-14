3fe8a86eea63da9590da15acdfbdc7619b43d933
NISHIKINO_Wuzhu <wx_11d33967b1974ea1b536e15c9f375bf4@git.code.tencent.com>
2020-04-14 19:56:49 +0800

    补充

---
+++ src/main/java/com/nchu/rebooks/RebooksApplication.java
@@ -0,0 +1,13 @@
+package com.nchu.rebooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RebooksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RebooksApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nchu/rebooks/RebooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nchu.rebooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RebooksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
